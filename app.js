27abf50ee81ec8c80d063e95c015e96f70d6dc30
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-05-01 11:17:36 -0700

    added ajax call for cross domain communication

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 // $('#message').find('b').text('$2000');
 
-$.get('https://addb.absolutdrinks.com/drinks/?apiKey=791f7bb8531446d09af4f98a22a06424')
-.then(result => {
-  console.log(result);
-});
+// $.get('https://addb.absolutdrinks.com/drinks/?apiKey=791f7bb8531446d09af4f98a22a06424')
+// .then(result => {
+//   console.log(result);
+// });
+
+jQuery.ajax({
+     type: "GET",
+     url: 'https://addb.absolutdrinks.com/drinks/?apiKey=791f7bb8531446d09af4f98a22a06424',
+     dataType: "jsonp",
+     cache: false,
+     crossDomain: true,
+     processData: true,
+
+
+     success: function (data) {
+         alert(JSON.stringify(data));
+     },
+     error: function (XMLHttpRequest, textStatus, errorThrown) {
+         alert("error");
+     }
+ });
