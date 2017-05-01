6e7acf1d5afb0efea236f41b3c1938a29afeb741
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-05-01 10:15:42 -0700

    more changes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const pg = require('pg');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
-const requestProxy = require('express-request-proxy');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
