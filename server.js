b321a723923a2636c4a0c0b413c85890c1c587dc
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-05-01 10:05:57 -0700

    created server

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const fs = require('fs');
+const express = require('express');
+const pg = require('pg');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const requestProxy = require('express-request-proxy');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('.'));
+
+app.get('/', (request, response) => response.sendFile('index.html', { root: './' }));
+
+app.listen(PORT, function () {
+  console.log(`Your server is now running on port ${PORT}`);
+});
