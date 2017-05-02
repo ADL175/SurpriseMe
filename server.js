37ee76a74f692edebdc6588f918b48acf54789e9
ADL175 <armydavidlim@gmail.com>
2017-05-01 17:43:58 -0700

    WIP not populating table data

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 const drinkKey = '791f7bb8531446d09af4f98a22a06424';
 const conString = process.env.DATABASE_URL + 'drinks';
 const client = new pg.Client(conString);
+const request = require('superagent');
+const drinksURL = 'https://addb.absolutdrinks.com/drinks/?apiKey=791f7bb8531446d09af4f98a22a06424';
+client.connect();
+client.on('error', err => console.error('ERROR', err));
 
 
 app.use(bodyParser.json());
@@ -30,40 +34,43 @@ app.listen(PORT, function () {
 
 function loadDrinks() {
   console.log('load drinks says hi');
-  $.get('https://addb.absolutdrinks.com/drinks/?apiKey=791f7bb8531446d09af4f98a22a06424', (err, fd) => {
-    JSON.parse(fd.toString()).forEach(ele => {
-      client.query(
-        `INSERT INTO
-        drinks(id, name, description, ingredients, drinkTypes)
-        VALUES ($1, $2, $3, $4, $5)
-        ON CONFLICT DO NOTHING`,
-        [ele.id, ele.name, ele.description, ele.ingredients, ele.drinkTypes]
-      )
-      .catch(console.error);
-      console.log(ele);
-    })
-  })
-}
+  request.get(drinksURL)
+  .then(result => {
+    let drinksData = result.body;
+    console.log(drinksData.forEach(ele => {ele.name}));
+      // console.log(JSON.parse(drinksData));
+
+    // drinksData.map(ele => {
+      // console.log(ele);
+      // client.query(
+      //   `INSERT INTO
+      //   drinks(id, name, description, ingredients, drinkTypes)
+      //   VALUES ($1, $2, $3, $4, $5)
+      //   ON CONFLICT DO NOTHING`,
+      //   [ele.id, ele.name, ele.description, ele.ingredients, ele.drinkTypes]
+      // )
+      // .catch(console.error);
+      // console.log(ele);
+    // })
+  }
+)}
 
 function loadDB() {
-  client.query('SELECT * FROM haha')
-    .then(console.log)
-    .catch(console.error);
 
-  // client.query(`
-  //   CREATE TABLE IF NOT EXISTS
-  //   drinks (
-  //     drink_id SERIAL PRIMARY KEY,
-  //     id VARCHAR(255) UNIQUE NOT NULL,
-  //     name VARCHAR(255) UNIQUE NOT NULL,
-  //     description VARCHAR(255) UNIQUE NOT NULL,
-  //     ingredients VARCHAR(255) UNIQUE NOT NULL,
-  //     drinkTypes VARCHAR(255) UNIQUE NOT NULL
-  //   );
-  //   `)
-  // // .then(loadDrinks)
-  // .catch(console.error);
-  // console.log('loadDB says hi');
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    drinks (
+      drink_id SERIAL PRIMARY KEY,
+      id VARCHAR(255) UNIQUE NOT NULL,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description VARCHAR(255) UNIQUE NOT NULL,
+      ingredients VARCHAR(255) UNIQUE NOT NULL,
+      drinkTypes VARCHAR(255) UNIQUE NOT NULL
+    );
+    `)
+  .then(loadDrinks)
+  .catch(console.error);
+
 }
 
 loadDB();
