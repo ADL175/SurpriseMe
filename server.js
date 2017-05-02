3d1b6a1812db7ae2106171b3ea022fe2faecfbf5
ADL175 <armydavidlim@gmail.com>
2017-05-02 10:56:03 -0700

    WIP success in getting JSON to populate database, need to add ingredients

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const fs = require('fs');
+const fs = require('fs');
 const express = require('express');
 const pg = require('pg');
 const app = express();
@@ -32,26 +32,37 @@ app.listen(PORT, function () {
 //////// ** DATABASE LOADERS ** ////////
 ////////////////////////////////////////
 
+
+//           client.query(
+//             `INSERT INTO
+//             drinks(id, name, description, ingredients, drinkTypes)
+//             VALUES ($1, $2, $3, $4, $5)
+//             ON CONFLICT DO NOTHING`,
+//             [ele.id, ele.name, ele.description, ele.ingredients, ele.drinkTypes]
+
+
+
+
 function loadDrinks() {
   console.log('load drinks says hi');
   request.get(drinksURL)
   .then(result => {
-    let drinksData = result.body;
-    console.log(drinksData.forEach(ele => {ele.name}));
+    let drinksData = result.body.result;
+    // console.log(result.body);
+    // console.log(drinksData);
+    // console.log(drinksData.forEach(ele => {ele.name}));
       // console.log(JSON.parse(drinksData));
 
-    // drinksData.map(ele => {
-      // console.log(ele);
-      // client.query(
-      //   `INSERT INTO
-      //   drinks(id, name, description, ingredients, drinkTypes)
-      //   VALUES ($1, $2, $3, $4, $5)
-      //   ON CONFLICT DO NOTHING`,
-      //   [ele.id, ele.name, ele.description, ele.ingredients, ele.drinkTypes]
-      // )
-      // .catch(console.error);
-      // console.log(ele);
-    // })
+    drinksData.map(ele => {
+      client.query(
+        `INSERT INTO
+        drinks(id, name, ingredients)
+        VALUES ($1, $2, $3)
+        ON CONFLICT DO NOTHING`,
+        [ele.id, ele.name, ele.ingredients]
+      )
+      .catch(console.error);
+    })
   }
 )}
 
@@ -61,11 +72,9 @@ function loadDB() {
     CREATE TABLE IF NOT EXISTS
     drinks (
       drink_id SERIAL PRIMARY KEY,
-      id VARCHAR(255) UNIQUE NOT NULL,
-      name VARCHAR(255) UNIQUE NOT NULL,
-      description VARCHAR(255) UNIQUE NOT NULL,
-      ingredients VARCHAR(255) UNIQUE NOT NULL,
-      drinkTypes VARCHAR(255) UNIQUE NOT NULL
+      id VARCHAR(1024) UNIQUE NOT NULL,
+      name VARCHAR(1024) UNIQUE NOT NULL,
+      ingredients VARCHAR(1024) UNIQUE NOT NULL
     );
     `)
   .then(loadDrinks)
